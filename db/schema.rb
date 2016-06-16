405b9490ae78ea9c802f69f13dbc54ec427f3b4e
RHOPRHH <rhoprhh@gmail.com>
2016-06-16 11:01:42 -0400

    created migration to change amount from int to float

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615205755) do
+ActiveRecord::Schema.define(version: 20160616145856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20160615205755) do
   create_table "expenses", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.integer  "amount"
+    t.float    "amount"
     t.integer  "group_id"
     t.integer  "attendee_id"
     t.datetime "created_at",  null: false
